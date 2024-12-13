a3ed9589fd3d3182914c293d1865fa1967e80272
Bernardo Ozelame Spoltti <104913275+BernardoOzelame@users.noreply.github.com>
2024-12-13 11:40:04 -0300

    Altera escrita do blade enviado no e-mail

--- resources/views/emails/alert_crypto.blade.php
+++ resources/views/emails/alert_crypto.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Alerta de Criptomoeda</title>
+    <title>Alerta configurado com sucesso!</title>
 </head>
 <body>
     <h1>Olá, {{ $userName }}!</h1>
-    <p>O valor da moeda <strong>{{ $coinName }}</strong> atingiu o limite configurado: <strong>{{ $alertValue }}</strong>.</p>
+    <p>O alerta para a moeda <strong>{{ $coinName }}</strong> foi configurado, com o valor em: <strong>{{ $alertValue }}</strong>.</p>
+    <p>Assim que percebermos que essa moeda atingiu o valor previamente configurado, vamos lhe enviar outro e-mail o mais rápido possível!</p>
     <p>Obrigado por usar nosso serviço!</p>
 </body>
 </html>
